9443bd278155daacabdfe9cb4d153e12a1d03d53
Haris Iqbal <128324051+haris-iqbal-h@users.noreply.github.com>
2023-04-29 22:11:48 +0500

    home and form component add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
  import Graph from '../components/Graph'
+import Form from '../components/Form'
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
                 {/* chart */}
                 <Graph />
                 {/* form */}
+                <Form/>
             </div>
         </div>
     </>
